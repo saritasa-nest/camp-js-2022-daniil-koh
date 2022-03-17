fda901bda570ee748554d33e8377bf4362fe30ac
Данил <daniil.koh@saritasa.com>
2022-03-17 09:21:24 +0700

    Init react boilerplate

---
+++ react/src/features/posts/routes.tsx
@@ -0,0 +1,16 @@
+import { lazy } from 'react';
+import { Navigate, RouteObject } from 'react-router-dom';
+
+const PostsPage = lazy(() => import('./pages/PostsPage').then(module => ({ default: module.PostsPage })));
+
+export const postsRoutes: RouteObject[] = [
+  {
+    path: 'posts',
+    element: <PostsPage />,
+  },
+  {
+    path: '*',
+    element: <Navigate to="PostsPage" />,
+  },
+];
+~
